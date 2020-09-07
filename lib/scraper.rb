f483818ec69aee2d9c031a21de11126084a323f1
NickRylander <nick.rylander@outlook.com>
2020-09-07 10:29:26 -0700

    Found Titles and Details

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,21 +2,18 @@ class Scraper
 
     def get_path
         doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-        todays_workout = doc.css("div.content-container .show a").first.text.strip
-
-        binding.pry
+        all_workout_titles = doc.css("div.content-container .show a").text
+        todays_workout_title = doc.css("div.content-container .show a").first.text.strip
+        all_workout_details = doc.css("div.content-container .row .col-sm-6").text
+        todays_workout_details = doc.css("div.content-container .row .col-sm-6").first.text.strip
+        puts todays_workout_title
+        puts todays_workout_details
     end
-
-
-        # URL = "https://www.crossfit.com/workout/"
-
-    # def get_workouts
-    #     uri = URI.parse(URL)
-    #     response = Net::HTTP.get_response(uri)
-    #     response.body
-    #     JSON.parse(response.body)
-    #     binding.pry
-    # end
 end
 
 # ROW EACH!!! 
+
+# all_workout_titles = doc.css("div.content-container .show a").text
+# todays_workout_title = doc.css("div.content-container .show a").first.text.strip
+# all_workout_details = doc.css("div.content-container .row .col-sm-6").text
+# todays_workout_title = doc.css("div.content-container .row .col-sm-6").first.text.strip
