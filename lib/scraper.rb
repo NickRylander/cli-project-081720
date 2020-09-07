4cdbb14e55e8ec60e0a22b6a340e707f66a5472d
NickRylander <nick.rylander@outlook.com>
2020-09-07 12:36:12 -0700

    working each loop

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,19 +1,24 @@
 class Scraper
 
+    WORKOUT_URL = "https://www.crossfit.com/workout"
+    RELATIVE_URL = "https://www.crossfit.com"
     def get_path
-        doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-        all_workout_titles = doc.css("div.content-container .show a").text
-        todays_workout_title = doc.css("div.content-container .show a").first.text.strip
-        all_workout_details = doc.css("div.content-container .row .col-sm-6").text
-        todays_workout_details = doc.css("div.content-container .row .col-sm-6").first.text.strip
-        puts todays_workout_title
-        puts todays_workout_details
+        html = open(WORKOUT_URL)
+        doc = Nokogiri::HTML(open(html))
+        doc.css(".row.content-container").each do | workout |
+            date = workout.css(".show a").text.strip
+            url = workout.css("a").attr("href").value
+            binding.pry
+        end
     end
+
+
 end
 
-# ROW EACH!!! 
+# ROW.CONTAINER EACH
+# .ROW.CONTENT-CONTAINER
 
 # all_workout_titles = doc.css("div.content-container .show a").text
 # todays_workout_title = doc.css("div.content-container .show a").first.text.strip
 # all_workout_details = doc.css("div.content-container .row .col-sm-6").text
-# todays_workout_title = doc.css("div.content-container .row .col-sm-6").first.text.strip
+# todays_workout_details = doc.css("div.content-container .row .col-sm-6").first.text.strip
