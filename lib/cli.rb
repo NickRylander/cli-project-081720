8ada10455c10ce67fab107532e34fecae317bcf9
NickRylander <nick.rylander@outlook.com>
2020-09-07 18:34:25 -0700

    Finished MENU

--- lib/cli.rb
+++ lib/cli.rb
@@ -12,7 +12,7 @@ class CLI
         end
     end
     def menu
-        puts "Please type a number to see the workout!"
+        puts "Please type a number to see the workout for that day!"
         input = gets.chomp
         if !input.to_i.between?(1, Workout.all.length)
             puts "Please choose a number from the list! ;)"
@@ -22,12 +22,26 @@ class CLI
             workout = Workout.all[input.to_i-1]
             display_details(workout)
         end
+        puts "Haven't had enough?! Want to see another?"
+        puts "Type \"Y\" for more soul crushing or \"N\" for soul already crushed."
+        another_workout = gets.chomp.downcase
+        if another_workout == "y"
+            puts "You're crazy, but I like it."
+            list_workouts
+            menu
+        elsif another_workout == "n"
+            puts "Go take a nap!"
+            exit
+        else
+            puts "You're not making any sense."
+            list_workouts
+            menu
+        end
     end
     def display_details(workout)
         Scraper.scrape_details(workout)
-        puts "Just incase you forgot the day you wanted, it's #{workout.day}!"
-        # puts "#{workout.day}"
-        puts "Workout Details:"
+        puts "Just incase you forgot the day you wanted, it was #{workout.day}!\n\n"
+        puts "Workout Details:\n\n"
         puts "#{workout.details}"
     end
 end
